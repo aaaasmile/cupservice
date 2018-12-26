cd25a6488cdd11b63b5afc301693243f2731a2b2
aaaasmile <testhaus@gmx.at>
2018-12-26 20:10:31 +0100

    show info

--- static/jsx/app.jsx
+++ static/jsx/app.jsx
@@ -2,11 +2,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      isLoggedIn: false
     };
     console.log("APP is build in constructor")
     this.onInfoClick = this.onInfoClick.bind(this)
     this.commanderRef = React.createRef();
+    history.pushState({ info: false }, ``, `./#`)
+    this.activateOnHistory()
   }
 
   onInfoClick() {
@@ -14,49 +16,37 @@ class App extends React.Component {
     this.setNewStateHist({ info: true }, `info`, `./#info`)
   }
 
-  clearResult() {
-    this.setState({})
-  }
-
-  restoreOnHistory() {
+  activateOnHistory() {
     window.addEventListener('popstate', e => {
       console.log('browser go back')
-      this.setNewState(e.state)
+      this.setState(e.state)
     })
   }
 
   setNewStateHist(obj, title, url) {
-    this.clearResult()
     this.setState(obj, () => history.pushState(this.state, title, url))
   }
 
   render() {
+    let detControl
+    if (this.state.info) {
+      detControl = <InfoControl info={this.state.info}></InfoControl>
+    }
     return (
       <div>
         <button className="ui right floated button icon" onClick={this.onInfoClick}><i className="info circle icon"></i></button>
-        {this.props.info ? <Info info={this.state.info}></Info> : null}
+        {detControl}
       </div>
     )
   }
 }
 
-///////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////// INFO
-///////////////////////////////////////////////////////////////////////////////////
-
-class Info extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <h2 className="ui dividing header">Info su...</h2>
-      </div>
-    )
-  }
+function InfoControl(props) {
+  return (
+    <div>
+      <h2 className="ui dividing header">Info su...</h2>
+    </div>
+  )
 }
 
 
