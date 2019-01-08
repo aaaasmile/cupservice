a6de9e13f01397649cff5384e3d0fd756ffb8a82
aaaasmile <testhaus@gmx.at>
2019-01-08 22:21:42 +0100

    first canvas draw

--- static/js/custom/games/brisc-base/brisc-base-gfx.js
+++ static/js/custom/games/brisc-base/brisc-base-gfx.js
@@ -8,8 +8,46 @@ export class BriscBaseGfx {
     this.canvasNode = document.getElementById(canvid)
     this.waitUntilCanvasReady(10, canvid, () => {
       console.log('Canvas is ready', this.canvasNode)
+      this.testSomeCanvas(canvid)
     })
 
+  }
+
+  testSomeCanvas(canvid) {
+    console.log('Canvas initialization');
+    this.mainStage = new createjs.Stage(canvid);
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    this.mainStage.addChild(circle);
+
+    var title = new createjs.Text();
+    title.text = 'Briscola Rocks!';
+    title.font = '14px Georgia';
+    title.color = 'black';
+    title.x = 10;
+    title.y = 10;
+    this.mainStage.addChild(title);
+
+    // this.imgTmp = new Image();
+    // this.imgTmp.src = "assets/carte/piac/01_denar.png";
+    // //this.imgTmp.onload = this.imageLoaded;
+    // this.imgTmp.onload = () => {
+    //   console.log('Example image Loaded');
+    //   let card = new createjs.Bitmap(this.imgTmp);
+    //   //card.x = 30;
+    //   //card.y = 20;
+    //   this.mainStage.addChild(card);
+    //   this.mainStage.update();
+    // }
+
+    // let card = new createjs.Bitmap("/dist/res/carte/piac/01_denar.png");
+    // card.x = 30;
+    // card.y = 20;
+    // this.mainStage.addChild(card);
+
+    this.mainStage.update();
   }
 
   waitUntilCanvasReady(time, canvid, cb_ready) {
@@ -21,7 +59,9 @@ export class BriscBaseGfx {
     setTimeout(() => {
       this.canvasNode = document.getElementById(canvid);
       console.log('Canvas node is', this.canvasNode)
-      if (!this.canvasNode) {
+      if (this.canvasNode) {
+        cb_ready()
+      }else{
         this.waitUntilCanvasReady(time + 10, canvid, cb_ready)
       }
     }, time)
