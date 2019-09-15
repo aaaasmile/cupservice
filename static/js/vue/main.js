066da3a9d22ae9df11d564ff594073a1b8a8e8d4
aaaasmile <testhaus@gmx.at>
2019-09-15 17:34:54 +0200

    update

--- static/js/vue/main.js
+++ static/js/vue/main.js
@@ -1,5 +1,12 @@
 
+Vue.component('offlinegames', {
+  template: '<div class="ui segment"><h2 class="ui header">Giochi disponibili contro il computer</h2></div>'
+})
 
+
+Vue.component('appview', {
+  template: '<div class="ui container"><offlinegames/></div>'
+})
 
 
 export const app = new Vue({
