95cb63d4a8a88af2c52df0906f487509e07a8fd2
aaaasmile <testhaus@gmx.at>
2019-09-15 17:41:24 +0200

    update

--- static/js/vue/main.js
+++ static/js/vue/main.js
@@ -1,21 +1,23 @@
 
 Vue.component('offlinegames', {
-  template: '<div class="ui segment"><h2 class="ui header">Giochi disponibili contro il computer</h2></div>'
+  template: `
+  <div class="ui segment">
+    <h2 class="ui header">Giochi disponibili contro il computer</h2>
+  </div>`
 })
 
 
 Vue.component('appview', {
-  template: '<div class="ui container"><offlinegames/></div>'
+  template: ` 
+  <div class="ui container">
+    <offlinegames/>
+  </div>`
 })
 
 
 export const app = new Vue({
   el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  },
-  //template: '<p>Vue message is {{message}}</p>'
   template: '<appview/>'
 })
 
-console.log('Main is here!')
+console.log('Main Vue is here!')
