9281d8d11972366f931f2a833d5524c5e4303eaa
aaaasmile <testhaus@gmx.at>
2019-09-15 21:23:19 +0200

    add about

--- static/js/vue/comp/about-page.js
+++ static/js/vue/comp/about-page.js
@@ -2,5 +2,7 @@ export const  AboutPage = Vue.component('about', {
   template: ` 
   <div class="ui container">
     <h2>About Cuperativa</h2>
+    <p>App per giocare alle carte by Invido.it.</p>
+    <a class="ui button icon ok" href="/cup/" />
   </div>`
 })
