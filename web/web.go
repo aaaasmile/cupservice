382fd590b6481f2bfdc522692d85b5f1d9ab8883
aaaasmile <testhaus@gmx.at>
2019-11-30 02:18:44 +0100

    update proxy

--- web/web.go
+++ web/web.go
@@ -32,11 +32,13 @@ func RunService(configfile string) {
 	cupServURL = strings.Replace(cupServURL, "127.0.0.1", "localhost", 1)
 	log.Println("Server started with URL ", serverurl)
 	log.Println("Try this url: ", cupServURL)
+	log.Println("Try this url: ", "http://localhost:5571//start/myapp")
 
 	http.Handle(conf.Current.RootURLPattern+"static/", http.StripPrefix(conf.Current.RootURLPattern+"static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc(conf.Current.RootURLPattern, rest.CupAPiHandler)
 	http.HandleFunc("/websocket", ws.WsHandler)
-	http.HandleFunc("/redirect", proxyOrfHandler)
+	http.HandleFunc("/proxed/", proxySecondDomainHandler)
+	http.HandleFunc("/start/myapp", startMyAppfHandler)
 
 	srv := &http.Server{
 		Addr:         serverurl,
@@ -72,14 +74,19 @@ loop:
 	log.Println("Bye, service")
 }
 
-func proxyOrfHandler(w http.ResponseWriter, r *http.Request) {
-	// In browser use: http://localhost:5571/redirect
-	//url, _ := url.Parse("http://wetter.orf.at/wien/")
+func startMyAppfHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Start my app")
+	r.URL.Path = "proxed"
+	proxySecondDomainHandler(w, r)
+}
+
+func proxySecondDomainHandler(w http.ResponseWriter, r *http.Request) {
 	url, _ := url.Parse("http://localhost:5591/")
-	r.URL.Path = "proxed" // importante viene aggiunto alla request. La url sopra è sempre terminata da uno /
+	//r.URL.Path = "proxed" // importante viene aggiunto alla request. La url sopra è sempre terminata da uno /
 	log.Printf("proxy handler %s on path %s", url, r.URL.Path)
-	// create the reverse proxy
+
 	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy.Transport = DebugTransport{}
 
 	// Update the headers to allow for SSL redirection
 	r.URL.Host = url.Host
@@ -91,3 +98,31 @@ func proxyOrfHandler(w http.ResponseWriter, r *http.Request) {
 	// Note that ServeHttp is non blocking and uses a go routine under the hood
 	proxy.ServeHTTP(w, r)
 }
+
+type DebugTransport struct{}
+
+func (DebugTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	b, err := httputil.DumpRequestOut(r, false)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(string(b))
+	return http.DefaultTransport.RoundTrip(r)
+}
+
+// Second server
+// func proxedHandler(w http.ResponseWriter, r *http.Request) {
+// 	tok := r.Header.Get("jwttoken")
+// 	log.Println("Request to proxed, token is ", tok)
+// 	//w.Write([]byte(fmt.Sprintf("Hello from proxed! Your token is %s", tok)))
+
+// 	log.Println("Try to redirect...")
+// 	http.Redirect(w, r, "/proxed/App", 302)
+
+// }
+
+// func proxedAppHandler(w http.ResponseWriter, r *http.Request) {
+// 	tok := r.Header.Get("jwttoken")
+// 	log.Println("Request to APP proxed, token is ", tok)
+// 	w.Write([]byte(fmt.Sprintf("You are in APP! Your token is %s", tok)))
+// }
