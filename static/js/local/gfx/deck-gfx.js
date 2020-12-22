ccf48391713581615831cae5ef0a23f8f8734c9b
aaaasmile <testhaus@gmx.at>
2020-12-23 00:33:29 +0100

    show the deck

--- static/js/local/gfx/deck-gfx.js
+++ static/js/local/gfx/deck-gfx.js
@@ -1,9 +1,9 @@
 export class DeckGfx {
   constructor(z_ord) {
-    this.deckSprite = []
-    this.z_ord = z_ord
-    this.briscola = null
-    this.container = new PIXI.Container()
+    this._deckSprite = []
+    this._z_ord = z_ord
+    this._briscola = null
+    this._container = new PIXI.Container()
   }
 
   Build(numCardsOnDeck, deckItemTexture, briscolaTexture) {
@@ -14,36 +14,43 @@ export class DeckGfx {
     for (let index = 0; index < numCardsOnDeck; index++) {
       let sprite = new PIXI.Sprite(deckItemTexture)
       sprite.position.set(x, y)
-      this.deckSprite.push(sprite)
-      this.container.addChild(sprite)
+      this._deckSprite.push(sprite)
+      this._container.addChild(sprite)
       x += 1
       y += 1
     }
 
     if (briscolaTexture) {
       let sprite = new PIXI.Sprite(briscolaTexture)
-      this.briscola = sprite
-      if (this.deckSprite.length > 0) {
-        let last = this.deckSprite[0]
+      this._briscola = sprite
+      if (this._deckSprite.length > 0) {
+        let last = this._deckSprite[0]
         y = last.y + last.height - ((last.height - last.width) / 2)
         x = last.x + last.width / 2
       }
       console.log('x,y of briscola', x, y)
       sprite.position.set(x, y)
       sprite.rotation = - Math.PI / 2.0
-      this.container.addChildAt(sprite, 0)
+      this._container.addChildAt(sprite, 0)
     }
-    return this.container
+    return this._container
+  }
+
+  Render(isDirty){
+    if (this._isDirty || isDirty){
+      console.log('*** render the deck ...')
+    }
+    this._isDirty = false
   }
 
   PopCard(num) {
     for (let index = 0; index < num; index++) {
-      let item = this.deckSprite.pop()
+      let item = this._deckSprite.pop()
       if (item) {
-        this.container.removeChild(item)
-      } else if (this.briscola) {
-        this.container.removeChild(this.briscola)
-        this.briscola = null
+        this._container.removeChild(item)
+      } else if (this._briscola) {
+        this._container.removeChild(this._briscola)
+        this._briscola = null
       }
     }
   }
