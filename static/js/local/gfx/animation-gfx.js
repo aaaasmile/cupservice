93e64046db7ef21f77eba8a5f196f5b2fc89285c
aaaasmile <testhaus@gmx.at>
2020-12-25 23:27:40 +0100

    update

--- static/js/local/gfx/animation-gfx.js
+++ static/js/local/gfx/animation-gfx.js
@@ -1,5 +1,5 @@
 export default (state, startComp, stopCom, fnTerm) => {
-    let _container =  new PIXI.Container()
+    let _container = new PIXI.Container()
     let _sprites = []
     let _started = false
     let _terminated = false
@@ -10,12 +10,24 @@ export default (state, startComp, stopCom, fnTerm) => {
     return {
         Update(delta) {
             if (_started) {
-                //console.log('Update animation')
+                //console.log('Update animation', delta)
                 _sprites.forEach(s => {
                     s.x += s.vx
                     s.y += s.vy
-                    if (s.x > s.end_x && s.y > s.end_y ){
-                        console.log('Animation terminated', s)
+                    if (s.vx > 0 && s.vy > 0) {
+                        if (s.x < (s.end_x - s.ini_x) / 2 + (s.end_x - s.ini_x) / 4) {
+                            s.vx++
+                            s.vy++
+                        } else {
+                            if (s.vx > 1) {
+                                s.vx--
+                                s.vy--
+                            }
+                        }
+                    }
+
+                    if (s.x > s.end_x && s.y > s.end_y) {
+                        //console.log('Animation terminated', s)
                         _fnTerm(_name)
                         _started = false
                         _terminated = true
@@ -23,19 +35,24 @@ export default (state, startComp, stopCom, fnTerm) => {
                 });
             }
         },
-        CheckForStart() { 
-            if (!_started){
+        CheckForStart() {
+            if (_terminated) {
+                return false
+            }
+            if (!_started) {
                 _started = true
                 return true
             }
-            return false 
+            return false
         },
         get_container() { return _container },
         is_terminated() { return _terminated },
         name() { return _name },
-        get_start_comp() {return _startComp},
-        get_stop_comp() {return _stopCom},
-        add_sprite(ss){
+        get_start_comp() { return _startComp },
+        get_stop_comp() { return _stopCom },
+        add_sprite(ss) {
+            ss.ini_x = ss.x
+            ss.ini_y = ss.y
             _sprites.push(ss)
             _container.addChild(ss)
         }
