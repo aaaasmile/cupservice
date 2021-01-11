06fab5bab742d6a6c819a9cd6786baa9f9aa10c5
aaaasmile <testhaus@gmx.at>
2021-01-11 22:04:22 +0100

    code unit is green again

--- static/js/local/core/core-state-manager.spec.js
+++ static/js/local/core/core-state-manager.spec.js
@@ -1,9 +1,10 @@
 describe('core state event base test', function () {
     it('table game full', () => {
         let coreStateManager = new cup.CoreStateManager('develop');
+        let b2core = new cup.CoreBriscolaBase(coreStateManager, 2, 61, 3);
         let tableStateCore = new cup.TableStateCore(coreStateManager, 2);
-        let playerErnesto = new cup.Player(null, coreStateManager, 'Ernesto');
-        let playerLuigi = new cup.Player(null, coreStateManager, 'Luigi');
+        let playerErnesto = new cup.Player(new cup.AlgBriscBase('Ernesto', b2core._deck_info), coreStateManager, 'Ernesto');
+        let playerLuigi = new cup.Player(new cup.AlgBriscBase('Luigi', b2core._deck_info), coreStateManager, 'Luigi');
 
         playerErnesto.sit_down(0);
         playerLuigi.sit_down(1);
--- static/js/local/games/brisc-base/core-brisc-base.spec.js
+++ static/js/local/games/brisc-base/core-brisc-base.spec.js
@@ -3,7 +3,7 @@ describe('brisc-base-core test', function () {
   prepareGame = (rnd_mgr) => {
     console.log('Prepare game for test')
     let coreStateManager = new cup.CoreStateManager('develop');
-    let b2core = new cup.CoreBriscolaBase(coreStateManager, 2, 61);
+    let b2core = new cup.CoreBriscolaBase(coreStateManager, 2, 61, 3);
     if (rnd_mgr) {
       b2core._rnd_mgr = rnd_mgr
     }
@@ -15,9 +15,9 @@ describe('brisc-base-core test', function () {
     });
 
 
-    let playerErnesto = new cup.Player(new cup.AlgBriscBase('Ernesto'), coreStateManager);
-    let playerLuigi = new cup.Player(new cup.AlgBriscBase('Luigi'), coreStateManager);
-    
+    let playerErnesto = new cup.Player(new cup.AlgBriscBase('Ernesto', b2core._deck_info, 'master'), coreStateManager);
+    let playerLuigi = new cup.Player(new cup.AlgBriscBase('Luigi', b2core._deck_info), coreStateManager);
+
     playerErnesto.sit_down(0);
     playerLuigi.sit_down(1);
     return b2core
@@ -35,7 +35,7 @@ describe('brisc-base-core test', function () {
 
   it('Punteggio giocata end', () => {
     let rnd_mgr = new cup.RndMgr();
-    rnd_mgr.set_predefined_deck( '_3s,_5c,_6b,_6c,_Ad,_Fb,_As,_3b,_Cd,_4c,_Fd,_5d,_4b,_6s,_5s,_2b,_Cs,_Fs,_7c,_Cb,_Rc,_7d,_2s,_5b,_7b,_Ab,_3d,_7s,_4d,_Rd,_2c,_2d,_Fc,_Ac,_3c,_Rb,_6d,_Rs,_4s,_Cc')
+    rnd_mgr.set_predefined_deck('_3s,_5c,_6b,_6c,_Ad,_Fb,_As,_3b,_Cd,_4c,_Fd,_5d,_4b,_6s,_5s,_2b,_Cs,_Fs,_7c,_Cb,_Rc,_7d,_2s,_5b,_7b,_Ab,_3d,_7s,_4d,_Rd,_2c,_2d,_Fc,_Ac,_3c,_Rb,_6d,_Rs,_4s,_Cc')
     rnd_mgr.set_predefined_player(1)
     let b2core = prepareGame(rnd_mgr)
     let coreStateManager = b2core._coreStateManager
@@ -77,7 +77,7 @@ describe('brisc-base-core test', function () {
   })
 
   it('Calcola punteggio', () => {
-    let b2core = new cup.CoreBriscolaBase(new cup.CoreStateManager('develop'), 2, 61);
+    let b2core = new cup.CoreBriscolaBase(new cup.CoreStateManager('develop'), 2, 61, 3);
     let points = b2core.calc_punteggio(['_As', '_7c'])
     expect(points).toEqual(11)
     points = b2core.calc_punteggio(['_3s', '_Fc'])
