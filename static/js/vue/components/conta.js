990920ca9f0935e448b66e0fc211b847dae65967
aaaasmile <testhaus@gmx.at>
2021-01-26 01:43:00 +0100

    update

--- static/js/vue/components/conta.js
+++ static/js/vue/components/conta.js
@@ -24,18 +24,40 @@ export default {
       console.log('Confirm the conta dialog')
       this.$store.commit('hideContaDialog')
     },
+    prevCard(){
+      console.log('Previous card')
+    },
+    nextCard(){
+      console.log('Next card')
+    }
   },
   template: `
-  <v-dialog v-model="dialogConta" persistent max-width="400">
+  <v-dialog v-model="dialogConta" persistent max-width="300">
     <v-card>
       <v-card-title class="headline">Conta le carte</v-card-title>
+      <v-main>
+        <v-container>
+          <v-row justify="center">
+            <v-col>
+              <v-img src="./static2/carte/piac/01_basto.png"> </v-img>
+            </v-col>
+            <v-col>
+              <v-img src="./static/assets/images/symbols/01_vuoto_trasp.png">
+              </v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
       <v-card-actions>
+        <v-btn icon  @click="prevCard">
+          <v-icon>mdi-skip-previous-circle-outline</v-icon>
+        </v-btn>
+        <v-btn icon @click="nextCard">
+          <v-icon>mdi-skip-next-circle-outline</v-icon>
+        </v-btn>
         <v-spacer></v-spacer>
-        <v-btn color="green darken-1" text @click="confirmConta"
-          >OK</v-btn
-        >
+        <v-btn color="green darken-1" text @click="confirmConta">OK</v-btn>
       </v-card-actions>
     </v-card>
-  </v-dialog>
-`
+  </v-dialog>`
 }
--- static/js/vue/components/conta.vue
+++ static/js/vue/components/conta.vue
@@ -1,12 +1,29 @@
 <template>
-  <v-dialog v-model="dialogConta" persistent max-width="400">
+  <v-dialog v-model="dialogConta" persistent max-width="300">
     <v-card>
       <v-card-title class="headline">Conta le carte</v-card-title>
+      <v-main>
+        <v-container>
+          <v-row justify="center">
+            <v-col>
+              <v-img src="./static2/carte/piac/01_basto.png"> </v-img>
+            </v-col>
+            <v-col>
+              <v-img src="./static/assets/images/symbols/01_vuoto_trasp.png">
+              </v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
       <v-card-actions>
+        <v-btn icon  @click="prevCard">
+          <v-icon>mdi-skip-previous-circle-outline</v-icon>
+        </v-btn>
+        <v-btn icon @click="nextCard">
+          <v-icon>mdi-skip-next-circle-outline</v-icon>
+        </v-btn>
         <v-spacer></v-spacer>
-        <v-btn color="green darken-1" text @click="confirmConta"
-          >OK</v-btn
-        >
+        <v-btn color="green darken-1" text @click="confirmConta">OK</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
