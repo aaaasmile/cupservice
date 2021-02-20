07745213ad2849891240daf0d1ea9005d011b59a
aaaasmile <testhaus@gmx.at>
2021-02-20 18:01:25 +0100

    update

--- static/js/vue/views/mainoptions.js
+++ static/js/vue/views/mainoptions.js
@@ -1,11 +1,12 @@
 export default {
     data() {
         return {
+            selgame: 'briscola'
         }
     },
     computed: {
         ...Vuex.mapState({
-      
+
         })
     },
     created() {
@@ -13,12 +14,21 @@ export default {
     mounted() {
     },
     methods: {
-      reserve(){
-      }
+        reserve() {
+        }
     },
     template: `
-  <v-row justify="center">
-    <v-col cols="12" sm="8"> </v-col>
-  </v-row>
-`
+  <v-card id="create">
+    <v-card-title>Opzioni e preferenze</v-card-title>
+    <v-container fluid>
+      <v-row class="child-flex">
+        <v-col cols="12" sm="6" md="4">
+          <v-subheader>Gioco selezionato</v-subheader>
+          <v-radio-group v-model="selgame" hide-details>
+            <v-radio value="briscola" label="Briscola"></v-radio>
+          </v-radio-group>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>`
 }
--- static/js/vue/views/mainoptions.vue
+++ static/js/vue/views/mainoptions.vue
@@ -1,5 +1,15 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="12" sm="8"> </v-col>
-  </v-row>
+  <v-card id="create">
+    <v-card-title>Opzioni e preferenze</v-card-title>
+    <v-container fluid>
+      <v-row class="child-flex">
+        <v-col cols="12" sm="6" md="4">
+          <v-subheader>Gioco selezionato</v-subheader>
+          <v-radio-group v-model="selgame" hide-details>
+            <v-radio value="briscola" label="Briscola"></v-radio>
+          </v-radio-group>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
